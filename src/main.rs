410edf7230fbbea2d0238adabdabe28b445ae89b
9ziggy9 <davidarogers@protonmail.com>
2023-11-15 23:39:54 -0800

    database pool created and simple get_streets() request

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@ use dotenv::dotenv;
 use std::env;
 use std::fmt;
 use warp:: Filter;
+use serde::{Serialize, Deserialize};
 use serde_json;
 use pretty_env_logger;
 use r2d2_sqlite::SqliteConnectionManager;
 use r2d2::Pool;
-use rusqlite::{params, Connection, Result};
-use rusqlite::NO_PARAMS;
+use rusqlite::Result;
 
 fn create_db_pool() -> Pool<SqliteConnectionManager> {
     let manager = SqliteConnectionManager::file("geo.db");
     Pool::new(manager).unwrap()
 }
 
+#[derive(Serialize, Deserialize)]
 struct Street {
     id: i32,
     name: String,
@@ -22,21 +23,17 @@ struct Street {
 }
 impl Street {
     async fn fetch_all(pool: Pool<SqliteConnectionManager>) -> Result<Vec<Street>> {
-        tokio::task::spawn_blocking(move || {
-            let conn = pool.get().unwrap();
-            let mut stmt = conn.prepare("SELECT * FROM streets;")?;
-            let street_iter = stmt.query_map(NO_PARAMS, |row| {
-                Ok(Street {
-                    id:   row.get(0)?,
-                    name: row.get(1)?,
-                    lat:  row.get(2)?,
-                    lon:  row.get(3)?,
-                })
-            })?;
-            street_iter.collect()
-        })
-        .await
-        .expect("Thread in fetch_all() for Streets panicked!")
+        let conn = pool.get().unwrap();
+        let mut stmt = conn.prepare("SELECT * FROM streets;")?;
+        let street_iter = stmt.query_map([], |row| {
+            Ok(Street {
+                id:   row.get(0)?,
+                name: row.get(1)?,
+                lat:  row.get(2)?,
+                lon:  row.get(3)?,
+            })
+        })?;
+        street_iter.collect()
     }
 }
 impl fmt::Display for Street {
@@ -51,7 +48,7 @@ async fn main() {
     dotenv().ok();
     pretty_env_logger::init();
 
-    let _db_pool = create_db_pool();
+    let db_pool = create_db_pool();
 
     let env_port = env::var("PORT")
         .unwrap()
@@ -61,18 +58,30 @@ async fn main() {
     println!("Starting server on port {}...", env_port);
 
     // GET
-    let route_hello = warp::path("hello")
+    let get_hello = warp::path("hello")
         .map(|| warp::reply::html("<h1>Hello, World!</h1>"));
 
+    let get_streets = warp::path("streets")
+        .and_then(move || {
+            let pool = db_pool.clone();
+            async move {
+                match Street::fetch_all(pool).await {
+                    Ok(streets) => Ok(warp::reply::json(&streets)),
+                    Err(_)      => Err(warp::reject::not_found()),
+                }
+            }
+        });
+
     // POST
-    let route_echo = warp::post()
+    let post_echo = warp::post()
         .and(warp::path("echo"))
         .and(warp::body::content_length_limit(1024 * 16))
         .and(warp::body::json())
         .map(|body: serde_json::Value| warp::reply::json(&body));
 
-    let routes = route_hello
-        .or(route_echo)
+    let routes = get_hello
+        .or(get_streets)
+        .or(post_echo)
         .with(warp::log("api"));
 
     warp::serve(routes)

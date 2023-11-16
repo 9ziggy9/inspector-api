808efa5cc9c339b37513b1a5d0eb785983247103
9ziggy9 <davidarogers@protonmail.com>
2023-11-15 21:37:37 -0800

    dependencies added: dotenv, tokio, warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, api!!");
+use dotenv::dotenv;
+use std::env;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let env_port = env::var("PORT")
+        .unwrap()
+        .parse()
+        .expect("PORT environment variable must be a number");
+
+    println!("Starting server on port {}...", env_port);
+
+    let hello_world = warp::path!("hello" / "world")
+        .map(|| warp::reply::html("<h1>Hello, World!</h1>"));
+
+    warp::serve(hello_world)
+        .run(([127, 0, 0, 1], env_port))
+        .await;
 }
